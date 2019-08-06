6816ae332b4c136602f7d95d8705c17ff9a09397
yz18710109979 <79241484@qq.com>
2019-08-06 10:46:56 +0800

    x

--- ex-service-reg/src/main/java/com/example/ex/service/reg/RegApplication.java
+++ ex-service-reg/src/main/java/com/example/ex/service/reg/RegApplication.java
@@ -1,5 +1,6 @@
 package com.example.ex.service.reg;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
@@ -10,4 +11,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = "com.example.ex.commons.mapper")
 public class RegApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(RegApplication.class, args);
+	}
 }
